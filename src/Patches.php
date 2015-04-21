89bc565092b6e3a94b80440d09af16775419fa0d
Cameron Eagans <me@cweagans.net>
2015-04-21 14:35:31 -0600

    Subscribing to install and update events and logging a notice to composer output when patches are found

--- src/Patches.php
+++ src/Patches.php
@@ -7,6 +7,64 @@
 
 namespace cweagans\Composer;
 
-class Patches {
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+class Patches implements PluginInterface, EventSubscriberInterface {
+
+  /**
+   * @var Composer $composer
+   */
+  protected $composer;
+  /**
+   * @var IOInterface $io
+   */
+  protected $io;
+
+  /**
+   * Apply plugin modifications to composer
+   *
+   * @param Composer    $composer
+   * @param IOInterface $io
+   */
+  public function activate(Composer $composer, IOInterface $io) {
+    $this->composer = $composer;
+    $this->io = $io;
+  }
+
+  /**
+   * Returns an array of event names this subscriber wants to listen to.
+   */
+  public static function getSubscribedEvents() {
+    return [
+      ScriptEvents::POST_INSTALL_CMD => "onInstallOrUpdate",
+      ScriptEvents::POST_UPDATE_CMD => "onInstallOrUpdate",
+    ];
+  }
+
+  /**
+   * @param Event $event
+   */
+  public function onInstallOrUpdate(Event $event) {
+    $extra = $this->composer->getPackage()->getExtra();
+    if (isset($extra['patches'])) {
+      $packages = $this->composer->getRepositoryManager()->getLocalRepository()->getPackages();
+      foreach ($packages as $package) {
+        /**
+         * @var PackageInterface $package
+         */
+        $package_name = $package->getName();
+        if (isset($extra['patches'][$package_name])) {
+          $this->io->write('<info>Found patches for ' . $package_name . '</info>');
+//          $install_path = $this->composer->getInstallationManager()->getInstallPath($package);
+        }
+      }
+    }
+  }
 
 }
