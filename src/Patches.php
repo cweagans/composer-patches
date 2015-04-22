936282f82a3e43be0df2cb8718b296e282535d0c
Cameron Eagans <me@cweagans.net>
2015-04-21 23:34:52 -0600

    Switching to package events

--- src/Patches.php
+++ src/Patches.php
@@ -8,12 +8,14 @@
 namespace cweagans\Composer;
 
 use Composer\Composer;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Package\PackageInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\Script\Event;
-use Composer\Script\ScriptEvents;
+use Composer\Installer\PackageEvent;
+use Composer\Installer\PackageEvents;
 
 class Patches implements PluginInterface, EventSubscriberInterface {
 
@@ -42,29 +44,49 @@ class Patches implements PluginInterface, EventSubscriberInterface {
    */
   public static function getSubscribedEvents() {
     return [
-      ScriptEvents::POST_INSTALL_CMD => "onInstallOrUpdate",
-      ScriptEvents::POST_UPDATE_CMD => "onInstallOrUpdate",
+      PackageEvents::POST_PACKAGE_INSTALL => "postInstall",
+      PackageEvents::POST_PACKAGE_UPDATE => "postInstall"
     ];
   }
 
   /**
-   * @param Event $event
+   * @param PackageEvent $event
+   * @throws Exception
    */
-  public function onInstallOrUpdate(Event $event) {
-    $extra = $this->composer->getPackage()->getExtra();
-    if (isset($extra['patches'])) {
-      $packages = $this->composer->getRepositoryManager()->getLocalRepository()->getPackages();
-      foreach ($packages as $package) {
-        /**
-         * @var PackageInterface $package
-         */
-        $package_name = $package->getName();
-        if (isset($extra['patches'][$package_name])) {
-          $this->io->write('<info>Found patches for ' . $package_name . '</info>');
-//          $install_path = $this->composer->getInstallationManager()->getInstallPath($package);
-        }
-      }
+  public function postInstall(PackageEvent $event) {
+    // Get the package object for the current operation.
+    $operation = $event->getOperation();
+    if ($operation instanceof InstallOperation) {
+      $package = $event->getOperation()->getPackage();
     }
+    elseif ($operation instanceof UpdateOperation) {
+      $package = $event->getOperation()->getTargetPackage();
+    }
+    else {
+      throw new Exception('Unknown operation: ' . get_class($operation));
+    }
+
+    /**
+     * @var PackageInterface $package
+     */
+    $extra = $this->composer->getPackage()->getExtra();
+    $package_name = $package->getName();
+    if (isset($extra['patches']) && isset($extra['patches'][$package_name])) {
+      $this->io->write('<comment>No patches found.</comment>');
+      return;
+    }
+
+    foreach ($extra['patches'][$package_name] as $description => $url) {
+      $message = '<comment>' . $description . ' (fetching from ' . $url . ')</comment>';
+      $this->io->write($message);
+    }
+
+
+    // Get the install path from the package object.
+//    $manager = $event->getComposer()->getInstallationManager();
+//    $install_path = $manager->getInstaller($package->getType())->getInstallPath($package);
+
+
   }
 
 }
