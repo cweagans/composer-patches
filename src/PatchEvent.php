8397f4dcd4374473dbeb1ad4ccc7a96ccb1bdd06
Fabian Franz <github@fabian-franz.de>
2015-08-24 09:24:02 -0700

    Add the new PatchEvent.

---
+++ src/PatchEvent.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @file
+ * Dispatch events when patches are applied.
+ */
+
+namespace cweagans\Composer;
+
+use Composer\EventDispatcher\Event;
+
+class PatchEvent extends Event {
+
+ /**
+  * @var PackageInterface $package
+  */
+ protected $package;
+ /**
+  * @var string $url
+  */
+ protected $url;
+ /**
+  * @var string $description
+  */
+ protected $description;
+
+  /**
+   * Constructs a PatchEvent object.
+   *
+   * @param string $eventName
+   * @param PackageInterface $package
+   * @param string $url
+   * @param string $description
+   */
+  public function __construct($eventName, PackageInterface $package, $url, $description) {
+    parent::__construct($eventName);
+    $this->package = $package;
+    $this->url = $url;
+    $this->description = $description;
+  }
+
+  /**
+   * Returns the package that is patched.
+   *
+   * @return PackageInterface
+   */
+  public function getPackage() {
+    return $package;
+  }
+
+  /**
+   * Returns the url of the patch.
+   *
+   * @return string
+   */
+  public function getUrl() {
+    return $url;
+  }
+
+  /**
+   * Returns the description of the patch.
+   *
+   * @return string
+   */
+  public function getDescription() {
+    return $description;
+  }
+
+}
