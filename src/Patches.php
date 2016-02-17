8bd545ecfebce782ffa3a2bcc0e6f22c9825260b
Mathieu Hélie <helie@floedesign.ca>
2016-02-17 16:33:42 -0500

    Restores patching from installed dependencies that are not updated.

--- src/Patches.php
+++ src/Patches.php
@@ -60,6 +60,7 @@ class Patches implements PluginInterface, EventSubscriberInterface {
     $this->eventDispatcher = $composer->getEventDispatcher();
     $this->executor = new ProcessExecutor($this->io);
     $this->patches = array();
+    $this->installedPatches = array();
   }
 
   /**
@@ -95,6 +96,9 @@ class Patches implements PluginInterface, EventSubscriberInterface {
 
       foreach ($packages as $package) {
         $extra = $package->getExtra();
+        if (isset($extra['patches'])) {
+          $this->installedPatches[$package->getName()] = $extra['patches'];
+        }
         $patches = isset($extra['patches']) ? $extra['patches'] : array();
         $tmp_patches = array_merge_recursive($tmp_patches, $patches);
       }
@@ -150,7 +154,16 @@ class Patches implements PluginInterface, EventSubscriberInterface {
         if (isset($extra['patches'])) {
           $this->patches = array_merge_recursive($this->patches, $extra['patches']);
         }
+        // Unset installed patches for this package
+        if(isset($this->installedPatches[$package->getName()])) {
+          unset($this->installedPatches[$package->getName()]);
+        }
       }
+    }
+
+    // Merge installed patches from dependencies that did not receive an update.
+    foreach ($this->installedPatches as $patches) {
+      $this->patches = array_merge_recursive($this->patches, $patches);
     }
 
     // If we're in verbose mode, list the projects we're going to patch.
@@ -165,7 +178,7 @@ class Patches implements PluginInterface, EventSubscriberInterface {
     // won't hurt anything, so we'll just stash it there.
     $this->patches['_patchesGathered'] = TRUE;
   }
-  
+
   /**
    * Get the patches from root composer or external file
    * @return Patches
