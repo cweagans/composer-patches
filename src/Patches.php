79ba8e215a0facaa558be43fef3637aee8a75d2c
Manuele Menozzi <manuele.menozzi@gmail.com>
2017-10-02 17:48:13 +0200

    Adds a comment about increased event priority

--- src/Patches.php
+++ src/Patches.php
@@ -72,6 +72,12 @@ class Patches implements PluginInterface, EventSubscriberInterface {
       ScriptEvents::PRE_UPDATE_CMD => array('checkPatches'),
       PackageEvents::PRE_PACKAGE_INSTALL => array('gatherPatches'),
       PackageEvents::PRE_PACKAGE_UPDATE => array('gatherPatches'),
+      // The following is a higher weight for compatibility with
+      // https://github.com/AydinHassan/magento-core-composer-installer and more generally for compatibility with
+      // every Composer plugin which deploys downloaded packages to other locations.
+      // In such cases you want that those plugins deploy patched files so they have to run after
+      // the "composer-patches" plugin.
+      // @see: https://github.com/cweagans/composer-patches/pull/153
       PackageEvents::POST_PACKAGE_INSTALL => array('postInstall', 10),
       PackageEvents::POST_PACKAGE_UPDATE => array('postInstall', 10),
     );
