2d5a0bb16616ca70f3dcfbb03685f9d4a408a811
Cameron Eagans <me@cweagans.net>
2017-10-19 18:11:57 -0700

    Reformat code to PSR-2

--- src/PatchEvent.php
+++ src/PatchEvent.php
@@ -10,61 +10,65 @@ namespace cweagans\Composer;
 use Composer\EventDispatcher\Event;
 use Composer\Package\PackageInterface;
 
-class PatchEvent extends Event {
+class PatchEvent extends Event
+{
 
- /**
-  * @var PackageInterface $package
-  */
- protected $package;
- /**
-  * @var string $url
-  */
- protected $url;
- /**
-  * @var string $description
-  */
- protected $description;
+    /**
+     * @var PackageInterface $package
+     */
+    protected $package;
+    /**
+     * @var string $url
+     */
+    protected $url;
+    /**
+     * @var string $description
+     */
+    protected $description;
 
-  /**
-   * Constructs a PatchEvent object.
-   *
-   * @param string $eventName
-   * @param PackageInterface $package
-   * @param string $url
-   * @param string $description
-   */
-  public function __construct($eventName, PackageInterface $package, $url, $description) {
-    parent::__construct($eventName);
-    $this->package = $package;
-    $this->url = $url;
-    $this->description = $description;
-  }
+    /**
+     * Constructs a PatchEvent object.
+     *
+     * @param string $eventName
+     * @param PackageInterface $package
+     * @param string $url
+     * @param string $description
+     */
+    public function __construct($eventName, PackageInterface $package, $url, $description)
+    {
+        parent::__construct($eventName);
+        $this->package = $package;
+        $this->url = $url;
+        $this->description = $description;
+    }
 
-  /**
-   * Returns the package that is patched.
-   *
-   * @return PackageInterface
-   */
-  public function getPackage() {
-    return $this->package;
-  }
+    /**
+     * Returns the package that is patched.
+     *
+     * @return PackageInterface
+     */
+    public function getPackage()
+    {
+        return $this->package;
+    }
 
-  /**
-   * Returns the url of the patch.
-   *
-   * @return string
-   */
-  public function getUrl() {
-    return $this->url;
-  }
+    /**
+     * Returns the url of the patch.
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
 
-  /**
-   * Returns the description of the patch.
-   *
-   * @return string
-   */
-  public function getDescription() {
-    return $this->description;
-  }
-
+    /**
+     * Returns the description of the patch.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
 }
--- src/PatchEvents.php
+++ src/PatchEvents.php
@@ -7,24 +7,24 @@
 
 namespace cweagans\Composer;
 
-class PatchEvents {
+class PatchEvents
+{
 
-  /**
-   * The PRE_PATCH_APPLY event occurs before a patch is applied.
-   *
-   * The event listener method receives a cweagans\Composer\PatchEvent instance.
-   *
-   * @var string
-   */
-  const PRE_PATCH_APPLY = 'pre-patch-apply';
+    /**
+     * The PRE_PATCH_APPLY event occurs before a patch is applied.
+     *
+     * The event listener method receives a cweagans\Composer\PatchEvent instance.
+     *
+     * @var string
+     */
+    const PRE_PATCH_APPLY = 'pre-patch-apply';
 
-  /**
-   * The POST_PATCH_APPLY event occurs after a patch is applied.
-   *
-   * The event listener method receives a cweagans\Composer\PatchEvent instance.
-   *
-   * @var string
-   */
-  const POST_PATCH_APPLY = 'post-patch-apply';
-
+    /**
+     * The POST_PATCH_APPLY event occurs after a patch is applied.
+     *
+     * The event listener method receives a cweagans\Composer\PatchEvent instance.
+     *
+     * @var string
+     */
+    const POST_PATCH_APPLY = 'post-patch-apply';
 }
