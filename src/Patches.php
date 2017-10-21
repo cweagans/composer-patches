6186fa0ffd10541b24b7dc0255fa1fbe50e479d6
Cameron Eagans <me@cweagans.net>
2017-10-20 17:10:11 -0700

    Use config system for exit-on-patch-failure

--- src/Patches.php
+++ src/Patches.php
@@ -309,10 +309,6 @@ class Patches implements PluginInterface, EventSubscriberInterface
      */
     public function postInstall(PackageEvent $event)
     {
-        // Check if we should exit in failure.
-        $extra = $this->composer->getPackage()->getExtra();
-        $exitOnFailure = getenv('COMPOSER_EXIT_ON_PATCH_FAILURE') || !empty($extra['composer-exit-on-patch-failure']);
-
         // Get the package object for the current operation.
         $operation = $event->getOperation();
         /** @var PackageInterface $package */
@@ -359,7 +355,7 @@ class Patches implements PluginInterface, EventSubscriberInterface
                     $e->getMessage() .
                     '</error>'
                 );
-                if ($exitOnFailure) {
+                if ($this->getConfig('exit-on-patch-failure')) {
                     throw new \Exception("Cannot apply patch $description ($url)!");
                 }
             }
