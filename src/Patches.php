d1b5af65e8afcf32e9ee11428204a2c393536daf
Christiaan de Die le Clercq <christiaan@uncinc.nl>
2018-01-25 13:09:22 +0100

    Change patch behaviour to be posix instead of a flag that breaks unix

--- src/Patches.php
+++ src/Patches.php
@@ -373,9 +373,9 @@ class Patches implements PluginInterface, EventSubscriberInterface {
     // the 'patch' command.
     if (!$patched) {
       foreach ($patch_levels as $patch_level) {
-        // --no-backup-if-mismatch here is a hack that fixes some
-        // differences between how patch works on windows and unix.
-        if ($patched = $this->executeCommand("patch %s --no-backup-if-mismatch -d %s < %s", $patch_level, $install_path, $filename)) {
+        // --posix is added to patch in order to make sure
+        // patch behaves the same way on windows, linux and unix.
+        if ($patched = $this->executeCommand("patch %s --posix -d %s < %s", $patch_level, $install_path, $filename)) {
           break;
         }
       }
