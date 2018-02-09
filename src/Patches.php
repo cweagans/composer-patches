28e0451c42e66fd0237408fdb901199be91187b4
Martijn Houtman <martijn@uncinc.nl>
2018-02-09 11:47:18 +0100

    - Replaced --no-backup-if-mismatch flag with --posix to fix FreeBSD compatiblity; - Added --batch flag to prevent interactive mode for patches that can not be applied in different -p levels;

--- src/Patches.php
+++ src/Patches.php
@@ -423,7 +423,7 @@ class Patches implements PluginInterface, EventSubscriberInterface
                 // --no-backup-if-mismatch here is a hack that fixes some
                 // differences between how patch works on windows and unix.
                 if ($patched = $this->executeCommand(
-                    "patch %s --no-backup-if-mismatch -d %s < %s",
+                    "patch %s --posix --batch -d %s < %s",
                     $patch_level,
                     $install_path,
                     $filename
