65be08f597e63c48bb752ecec4f7b7588b6360af
Cameron Eagans <me@cweagans.net>
2018-05-18 18:14:16 -0600

    Add dependency patch resolver + acceptance test

---
+++ src/Resolvers/DependencyPatches.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Contains \cweagans\Composer\Resolvers\DependencyPatches.
+ */
+
+namespace cweagans\Composer\Resolvers;
+
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\OperationInterface;
+use Composer\DependencyResolver\Operation\UpdateOperation;
+use Composer\Installer\PackageEvent;
+use Composer\Package\PackageInterface;
+use cweagans\Composer\Patch;
+use cweagans\Composer\PatchCollection;
+
+class DependencyPatches extends ResolverBase
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function resolve(PatchCollection $collection, PackageEvent $event)
+    {
+        $this->io->write('  - <info>Gathering patches from dependencies. This might take a minute.</info>');
+
+        $operations = $event->getOperations();
+        foreach ($operations as $operation) {
+            if ($operation->getJobType() == 'install' || $operation->getJobType() == 'update') {
+                // @TODO handle exception.
+                $package = $this->getPackageFromOperation($operation);
+                /** @var PackageInterface $extra */
+                $extra = $package->getExtra();
+                if (isset($extra['patches'])) {
+                    $patches = $this->findPatchesInJson($extra['patches']);
+                    foreach ($patches as $patch) {
+                        /** @var Patch $extra */
+                        $collection->addPatch($patch);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Get a Package object from an OperationInterface object.
+     *
+     * @param OperationInterface $operation
+     * @return PackageInterface
+     * @throws \Exception
+     *
+     * @todo Will this method ever get something other than an InstallOperation or UpdateOperation?
+     */
+    protected function getPackageFromOperation(OperationInterface $operation)
+    {
+        if ($operation instanceof InstallOperation) {
+            $package = $operation->getPackage();
+        } elseif ($operation instanceof UpdateOperation) {
+            $package = $operation->getTargetPackage();
+        } else {
+            throw new \Exception('Unknown operation: ' . get_class($operation));
+        }
+
+        return $package;
+    }
+}
