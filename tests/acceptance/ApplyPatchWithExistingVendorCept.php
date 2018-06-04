f244630de5b019877ce2654c0a2b9ab8c2dfcf5f
Andrew Berry <deviantintegral@gmail.com>
2018-06-04 15:19:45 -0400

    Complete failing test

--- tests/acceptance/ApplyPatchWithExistingVendorCept.php
+++ tests/acceptance/ApplyPatchWithExistingVendorCept.php
@@ -2,8 +2,8 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('apply a patch when the vendor directory contains an older patch');
 $I->amInPath(realpath(__DIR__ . '/fixtures/patches-existing-vendor-root'));
-$I->runShellCommand('composer install');
-$I->runShellCommand('cp -av web vendor ../patches-existing-vendor-update');
+$I->runShellCommand('composer install -n');
+$I->runShellCommand('mv web vendor ../patches-existing-vendor-update');
 $I->amInPath(realpath(__DIR__ . '/fixtures/patches-existing-vendor-update'));
-$I->runShellCommand('COMPOSER_DISCARD_CHANGES=1 composer install');
-//$I->canSeeFileFound('./vendor/drupal/drupal/.ht.router.php');
+$I->runShellCommand('COMPOSER_DISCARD_CHANGES=1 composer install -n');
+$I->canSeeFileFound('./web/modules/contrib/migrate_upgrade/drush.services.yml');
