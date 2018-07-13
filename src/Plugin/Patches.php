eecde1ab980a8435def8fc92f9f8b40b9f013dc0
Michael Stucki <michael@stucki.io>
2018-07-13 13:01:45 +0200

    Fixes #174: Compatibility with older Git versions

--- src/Plugin/Patches.php
+++ src/Plugin/Patches.php
@@ -457,6 +457,12 @@ class Patches implements PluginInterface, EventSubscriberInterface, Capable
      */
     protected function applyPatchWithGit($install_path, $patch_levels, $filename)
     {
+        // Do not use git apply unless the install path is itself a git repo
+        // @see https://stackoverflow.com/a/27283285
+        if (!is_dir($install_path . '/.git')) {
+            return false;
+        }
+
         foreach ($patch_levels as $patch_level) {
             if ($this->io->isVerbose()) {
                 $comment = 'Testing ability to patch with git apply.';
