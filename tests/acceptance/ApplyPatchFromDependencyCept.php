c32d533e40b00e0cbaf0c2140f6e312085d09efc
Cameron Eagans <me@cweagans.net>
2019-04-09 15:51:20 -0700

    Rip out dependency patch resolution

--- tests/acceptance/ApplyPatchFromDependencyCept.php
+++
@@ -1,6 +0,0 @@
-<?php
-$I = new AcceptanceTester($scenario);
-$I->wantTo('apply a patch defined in a dependency');
-$I->amInPath(realpath(__DIR__ . '/fixtures/apply-patch-from-dependency'));
-$I->runShellCommand('composer install');
-$I->canSeeFileFound('./vendor/drupal/drupal/.ht.router.php');
