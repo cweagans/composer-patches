8c4993c78d9e6dd1381111d6fc0529f8a68e94de
Cameron Eagans <me@cweagans.net>
2023-02-01 16:37:55 -0700

    Speed up acceptance tests substantially by using a simpler test repo to apply patches to

--- tests/acceptance/ExpandedDefinitionRequiredOnlyCept.php
+++ tests/acceptance/ExpandedDefinitionRequiredOnlyCept.php
@@ -10,4 +10,4 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('apply a patch to a package using the expanded definition format (required props only)');
 $I->amInPath(codecept_data_dir('fixtures/expanded-definition-required-only'));
 $I->runShellCommand('composer install');
-$I->canSeeFileFound('./vendor/drupal/core/.ht.router.php');
+$I->canSeeFileFound('./vendor/cweagans/composer-patches-testrepo/src/OneMoreTest.php');
--- tests/acceptance/PatchesFileApplyPatchFromWebCept.php
+++ tests/acceptance/PatchesFileApplyPatchFromWebCept.php
@@ -10,4 +10,4 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('modify a package using a patch downloaded from the internet (defined in patches file)');
 $I->amInPath(codecept_data_dir('fixtures/patches-file-patch-from-web'));
 $I->runShellCommand('composer install');
-$I->canSeeFileFound('./vendor/drupal/core/.ht.router.php');
+$I->canSeeFileFound('./vendor/cweagans/composer-patches-testrepo/src/OneMoreTest.php');
