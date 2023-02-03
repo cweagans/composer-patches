32c9100184f2b272219f108803650538e227e6e9
Cameron Eagans <me@cweagans.net>
2023-02-03 11:01:50 -0700

    Add package patch depth override list

--- src/Util.php
+++ src/Util.php
@@ -5,6 +5,25 @@ namespace cweagans\Composer;
 class Util
 {
     /**
+     * Contains pre-defined patch depths for various composer packages.
+     *
+     * You can open a pull request to add to this list. These values are *only* used
+     * if there is no patch depth specified on the patch itself (in composer.json
+     * or your patches file). Otherwise, the plugin will default to a depth of 1
+     * (-p1 as an argument to git or patch).
+     */
+    public static function getDefaultPackagePatchDepth($package): ?int
+    {
+        // You can open a pull request to add to this list.
+        // Please keep it alphabetized by vendor, then package name.
+        $packages = [
+            'drupal/core' => 2,
+        ];
+
+        return $packages[$package] ?? null;
+    }
+
+    /**
      * Recursively merge arrays without changing data types of values.
      *
      * Does not change the data types of the values in the arrays. Matching keys'
--- tests/unit/UtilTest.php
+++ tests/unit/UtilTest.php
@@ -13,6 +13,15 @@ use cweagans\Composer\Util;
 class UtilTest extends Unit
 {
     /**
+     * Tests getDefaultPackagePatchDepth.
+     */
+    public function testGetDefaultPackagePatchDepth()
+    {
+        $this->assertEquals(2, Util::getDefaultPackagePatchDepth('drupal/core'));
+        $this->assertNull(Util::getDefaultPackagePatchDepth('not-a-real-package'));
+    }
+
+    /**
      * Tests arrayMergeRecursiveDistinct.
      *
      * @dataProvider arrayMergeRecursiveDistinctDataProvider
