2419da6e3b88ac2ac20c53e611614d3cd00dcaf4
Cameron Eagans <me@cweagans.net>
2023-02-03 11:08:13 -0700

    Just use the project root as the plugin - no need to symlink stuff into place

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -10,26 +10,9 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class Acceptance extends Module
 {
-    /**
-     * Build a local repo that Composer can use as a path repository.
-     *
-     * @param array $settings
-     */
-    public function _beforeSuite($settings = [])
-    {
-        $filesystem = new Filesystem();
-        if ($filesystem->exists($this->_getPluginDir())) {
-            $this->_afterSuite();
-        }
-        $filesystem->mkdir($this->_getPluginDir());
-        $filesystem->symlink($this->_getProjectRoot() . '/src', $this->_getPluginDir() . '/src');
-        $filesystem->symlink($this->_getProjectRoot() . '/composer.json', $this->_getPluginDir() . '/composer.json');
-    }
-
     public function _afterSuite()
     {
         $filesystem = new Filesystem();
-        $filesystem->remove($this->_getPluginDir());
 
         $composerLockFiles = glob($this->_getFixturesDir() . '/*/composer.lock');
         $filesystem->remove($composerLockFiles);
@@ -41,16 +24,6 @@ class Acceptance extends Module
     protected function _getFixturesDir()
     {
         return codecept_data_dir('fixtures');
-    }
-
-    protected function _getPluginDir()
-    {
-        return $this->_getFixturesDir() . '/composer-patches';
-    }
-
-    protected function _getProjectRoot()
-    {
-        return dirname(__DIR__, 3);
     }
 
 }
