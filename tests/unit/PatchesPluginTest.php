ee00e4182739b335b51d2b0afa31b7963ea74221
Cameron Eagans <me@cweagans.net>
2023-02-07 22:01:01 -0700

    Add project-specific depth settings

--- tests/unit/PatchesPluginTest.php
+++ tests/unit/PatchesPluginTest.php
@@ -9,8 +9,10 @@ namespace cweagans\Composer\Tests\Unit;
 
 use Codeception\Test\Unit;
 use Composer\Composer;
+use Composer\EventDispatcher\EventDispatcher;
 use Composer\IO\NullIO;
 use Composer\Package\RootPackage;
+use cweagans\Composer\Patch;
 use cweagans\Composer\Plugin\Patches;
 
 class PatchesPluginTest extends Unit
@@ -31,5 +33,62 @@ class PatchesPluginTest extends Unit
         $composer->setPackage($package);
         $io = new NullIO();
         $plugin->activate($composer, $io);
+    }
+
+    /**
+     * Test the patch depth settings.
+     *
+     * @dataProvider guessDepthDataProvider
+     */
+    public function testGuessDepth(Patches $plugin, Patch $patch, int $expectedDepth)
+    {
+        $plugin->guessDepth($patch);
+        $this->assertEquals($expectedDepth, $patch->depth);
+    }
+
+    /**
+     * Provides data to testGuessDepth().
+     */
+    public function guessDepthDataProvider()
+    {
+        $package = new RootPackage('cweagans/composer-patches', '0.0.0.0', '0.0.0');
+        $package->setExtra([]);
+
+        $io = new NullIO();
+
+        $composer = new Composer();
+        $composer->setPackage($package);
+        $composer->setEventDispatcher(new EventDispatcher($composer, $io));
+
+        $plugin = new Patches();
+        $plugin->activate($composer, $io);
+
+        $patch = new Patch();
+        $patch->package = 'some/package';
+        yield 'global default depth' => [$plugin, $patch, 1];
+
+        $patch = new Patch();
+        $patch->depth = 123;
+        yield 'depth set on patch' => [$plugin, $patch, 123];
+
+        $patch = new Patch();
+        $patch->package = 'drupal/core';
+        yield 'depth set in global package override list' => [$plugin, $patch, 2];
+
+        $package = new RootPackage('cweagans/composer-patches', '0.0.0.0', '0.0.0');
+        $package->setExtra([
+            'composer-patches' => [
+                'package-depths' => [
+                    'some/package' => 234,
+                ],
+            ],
+        ]);
+        $composer->setPackage($package);
+        $plugin = new Patches();
+        $plugin->activate($composer, $io);
+
+        $patch = new Patch();
+        $patch->package = 'some/package';
+        yield 'depth set in project package override list' => [$plugin, $patch, 234];
     }
 }
