6374544c9957f4232046981ae3244cf3ad31c429
Cameron Eagans <me@cweagans.net>
2023-02-11 23:02:02 -0700

    Add test for patch that does not apply

---
+++ tests/acceptance/PatchDoesNotApplyCept.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @var \Codeception\Scenario $scenario
+ */
+
+use cweagans\Composer\Tests\AcceptanceTester;
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('see an error when I try to apply a patch that does not apply');
+$I->amInPath(codecept_data_dir('fixtures/patch-does-not-apply'));
+$I->runComposerCommand('install', ['-vvv'], false);
+$I->seeInComposerOutput("1 out of 1 hunk FAILED");
+$I->seeInComposerOutput("No available patcher was able to apply patch");
+$I->seeInComposerOutput("Exception trace");
