6c0bd11af562ac96d072aeb8edb91b63899e5170
Cameron Eagans <me@cweagans.net>
2023-02-12 21:49:04 -0700

    add test coverage for the dry run block

--- tests/_data/fixtures/freeform-patcher/kind-of-a-patcher.sh
+++ tests/_data/fixtures/freeform-patcher/kind-of-a-patcher.sh
@@ -3,6 +3,12 @@
 depth=$1
 path=$2
 file=$3
+dryrun=$4
+
+if [[ "$4" == "--dry-run" ]]; then
+    echo "dry run"
+    exit 0
+fi
 
 echo "not doing anything with the depth, but it was set to ${depth}"
 
