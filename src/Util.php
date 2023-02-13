4073f0111933afe5a29a86de2f55e748149d4171
Cameron Eagans <me@cweagans.net>
2023-02-12 21:41:13 -0700

    Remove unused method

--- src/Util.php
+++ src/Util.php
@@ -22,35 +22,4 @@ class Util
 
         return $packages[$package] ?? null;
     }
-
-    /**
-     * Recursively merge arrays without changing data types of values.
-     *
-     * Does not change the data types of the values in the arrays. Matching keys'
-     * values in the second array overwrite those in the first array, as is the
-     * case with array_merge.
-     *
-     * @param array $array1
-     *   The first array.
-     * @param array $array2
-     *   The second array.
-     * @return array
-     *   The merged array.
-     *
-     * @see http://php.net/manual/en/function.array-merge-recursive.php#92195
-     */
-    public static function arrayMergeRecursiveDistinct(array $array1, array $array2): array
-    {
-        $merged = $array1;
-
-        foreach ($array2 as $key => &$value) {
-            if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
-                $merged[$key] = self::arrayMergeRecursiveDistinct($merged[$key], $value);
-            } else {
-                $merged[$key] = $value;
-            }
-        }
-
-        return $merged;
-    }
 }
