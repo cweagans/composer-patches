91012ad1bcfd58f40f2d0f3b134b4a2a9a5dabaf
Cameron Eagans <me@cweagans.net>
2023-02-26 16:18:52 -0700

    Clean up so other tests do not fail

--- tests/acceptance/CustomComposerJsonFilenameCept.php
+++ tests/acceptance/CustomComposerJsonFilenameCept.php
@@ -23,3 +23,6 @@ $I->runShellCommand('composer patches-relock');
 $I->canSeeFileFound('./composer-a-patches.lock.json');
 $I->openFile('composer-a-patches.lock.json');
 $I->seeInThisFile('725f2631cb6a92c8c3ffc2e396e89f73b726869131d4c4d2a5903aae6854a260');
+
+// Clean up so other tests don't fail.
+putenv('COMPOSER');
